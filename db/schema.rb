d2c18816708a88860b163c07ed66e5d0f12cd22f
AcnologiatheEnd <18.chung.winston@ctkprep.org>
2021-03-05 16:30:57 -0500

    added schema for answered user questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_184659) do
+ActiveRecord::Schema.define(version: 2021_03_05_170314) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2021_01_13_184659) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.string "answered_questions"
   end
 
 end
